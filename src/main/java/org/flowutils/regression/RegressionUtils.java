cd0841e962cf2004094ba2cbacf2f5f08809157b
zzorn <zzorn@iki.fi>
2015-06-26 12:03:51 +0300

    Separate RingBuffer implementation is not needed, as RingBuffer now implements List.

--- src/main/java/org/flowutils/regression/RegressionUtils.java
+++ src/main/java/org/flowutils/regression/RegressionUtils.java
@@ -1,7 +1,6 @@
 package org.flowutils.regression;
 
 import org.flowutils.Check;
-import org.flowutils.collections.ringbuffer.RingBuffer;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -140,78 +139,6 @@ public final class RegressionUtils {
         float b = bUpper / bLower;
         float a = averageY - b * averageX;
 
-        return createRegressionFunction(exponential, a, b);
-    }
-
-    /**
-     * @param x0 x value of the first data entry.
-     * @param xStep x step between data entries.
-     * @param exponential if true, the data will be fitted to an exponential instead of a line.
-     * @param startIndex index in the data series to start from.
-     * @param count number of entries to use from the data series.
-     * @param data data to regression fit to a line or exponential.  null entries are ignored (the x is still increased for them)
-     * @return the regression function for the given data, or null if the data has less than two non-null values.
-     */
-    public static RegressionFunction calculateRegression(float x0,
-                                                         float xStep,
-                                                         boolean exponential,
-                                                         int startIndex,
-                                                         int count,
-                                                         RingBuffer<Float> data) {
-        Check.notZero(xStep, "xStep", 0);
-        Check.positiveOrZero(startIndex, "startIndex");
-
-
-        // Calculate x and y averages
-        float sumY = 0;
-        float sumX = 0;
-        int elementCount = 0;
-        float x = x0;
-        Iterator<Float> iterator = getIteratorAtStartIndex(startIndex, data);
-        for (int i = 0; i < count; i++) {
-            Float y = iterator.next();
-            if (y != null) {
-                if (exponential) y = getLogarithmOfY(y);
-                sumX += x;
-                sumY += y;
-                elementCount++;
-            }
-
-            x += xStep;
-        }
-
-        if (elementCount <= 1) return null;
-
-        float averageX = sumX / elementCount;
-        float averageY = sumY / elementCount;
-
-        //if (elementCount == 1) return new Line(averageY, 0); // With only one element, assume sideways line.
-
-        // Calculate regression
-        x = x0;
-        float bUpper = 0;
-        float bLower = 0;
-        iterator = getIteratorAtStartIndex(startIndex, data);
-        for (int i = 0; i < count; i++) {
-            Float y = iterator.next();
-            if (y != null) {
-                if (exponential) y = getLogarithmOfY(y);
-                bUpper += (x - averageX) * (y - averageY);
-                bLower += (x - averageX) * (x - averageX);
-            }
-
-            x += xStep;
-        }
-
-        if (bLower == 0) throw new IllegalStateException("Unexpectedly the divisor was zero");
-
-        float b = bUpper / bLower;
-        float a = averageY - b * averageX;
-
-        return createRegressionFunction(exponential, a, b);
-    }
-
-    private static RegressionFunction createRegressionFunction(boolean exponential, float a, float b) {
         // Last sanity check of the results
         if (Float.isNaN(a) || Float.isNaN(b) || Float.isInfinite(a) || Float.isInfinite(b)) {
             return null;
@@ -229,17 +156,6 @@ public final class RegressionUtils {
             // The results may be crap, but so was the inputs, and at least there won't be random infinite or NaN values or exceptions with bad data.
             return 0f;
         }
-    }
-
-    private static Iterator<Float> getIteratorAtStartIndex(int startIndex, Iterable<Float> data) {
-        // Get to correct place in data series
-        final Iterator<Float> iterator = data.iterator();
-        int index = 0;
-        while (index < startIndex) {
-            iterator.next();
-            index++;
-        }
-        return iterator;
     }
 
     private RegressionUtils() {
